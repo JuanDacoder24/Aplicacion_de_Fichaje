ce908ec412db7d135184bfc1c449f3d37dd48249
JuanDacoder24 <servellonrejasjuandavid@gmail.com>
2026-03-25 11:33:24 +0100

    probar el login en el front

--- Frontend/FrontFichajes/src/app/services/user-service.ts
+++ Frontend/FrontFichajes/src/app/services/user-service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUser } from '../interface/iuser';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  
+
+  private httpClient = inject(HttpClient)
+  private baseUrl = 'http://localhost:8080/api/login'
+
+  login(user: IUser): Promise<any>{
+    return firstValueFrom(this.httpClient.post<any>(this.baseUrl, user))
+  }
 }
